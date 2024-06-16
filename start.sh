8c1ff4b5018b9d052b8956b3f53626e50be0bc87
Israel1235513 <151475756+Israel1235513@users.noreply.github.com>
2024-06-16 12:14:31 -0300

    start.sh

---
+++ start.sh
@@ -0,0 +1,25 @@
+clear
+PRETO = "\033[0;30m"
+VERMELHO = "\033[0;31m"
+VERDE = "\033[0;32m"
+AZUL = "\033[0;34m"
+ROXO = "\033[0;35m"
+CIANO = "\033[0;36m"
+CINZA = "\033[0;37m"
+AMARELO = "\033[1;33m"
+BRANCO = " \033[1;37m"
+while :
+do 
+
+echo `${VERMELHO} 「 Vamos iniciar o seu painel. :D 」`
+echo `${VERMELHO} 「 Painel sendo iniciado com sucesso. :D 」`
+echo `${VERDE}██╗  ██╗██╗██╗     ██╗         ██╗    ██╗ █████╗`
+echo `${VERDE}██║ ██╔╝██║██║     ██║         ██║    ██║██╔══██╗`
+echo `${VERDE}█████╔╝ ██║██║     ██║         ██║ █╗ ██║███████║`
+echo `${VERDE}██╔═██╗ ██║██║     ██║         ██║███╗██║██╔══██║`
+echo `${VERDE}██║  ██╗██║███████╗███████╗    ╚███╔███╔╝██║  ██║`
+echo `${VERDE}╚═╝  ╚═╝╚═╝╚══════╝╚══════╝     ╚══╝╚══╝ ╚═╝  ╚═╝`
+node index.js
+
+sleep 1
+done
