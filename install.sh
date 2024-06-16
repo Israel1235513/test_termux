65cafd8faca6f8e1e47701afeddfc078a2f45bd8
Israel1235513 <151475756+Israel1235513@users.noreply.github.com>
2024-06-16 12:25:12 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -9,21 +9,21 @@ CINZA = "\033[0;37m"
 AMARELO = "\033[1;33m"
 BRANCO = " \033[1;37m"
 
-echo -e `${VERMELHO} 「 Preparando seu painel. :D 」`
-echo -e `${AZUL} 「 Está quase tudo pronto. :D 」`
-echo -e `${VERDE} 「 Vou instalar as dependencias isso pode demorar. 」`
-echo -e `${ROXO} 「 Vou instalar o python é muito importante. 」`
+echo -e `${VERMELHO}「 Preparando seu painel. :D 」`
+echo -e `${AZUL}「 Está quase tudo pronto. :D 」`
+echo -e `${VERDE}「 Vou instalar as dependencias isso pode demorar. 」`
+echo -e `${ROXO}「 Vou instalar o python é muito importante. 」`
 pkg install python -y
 pkg install python3 -y
 apt install python2 -y
-echo -e `${ROXO} 「 Vou instalar as dependencias agora. 」`
+echo -e `${ROXO}「 Vou instalar as dependencias agora. 」`
 pkg upgrade
 pkg install update
 pkg install upgrade
 pkg install ffmpeg
 apt update -y
 apt install wget -y
-echo -e `${ROXO} 「 Todas dependencias instaladas vou iniciar agora. 」`
+echo -e `${ROXO}「 Todas dependencias instaladas vou iniciar agora. 」`
 node start.sh
 
 sleep 1
